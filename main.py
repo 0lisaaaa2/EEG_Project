06a4e5f31e5bcc829eae66ff5e881a8d2509b113
Lisa Herrmann <herrmann.lisa02@gmail.com>
2025-12-11 15:18:07 +0100

    first version of bad channel annotation

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from config import subjects, tasks
 import s_00_fetch_data
 import s_01_filter
 import config
-import s_00_fetch_data, s_02_downsample
+import s_00_fetch_data, s_02_downsample, s_03_remove_bad_channels
 import matplotlib.pyplot as plt
 
 
@@ -11,6 +11,7 @@ if __name__ == "__main__":
         for task in tasks:
             raw = s_00_fetch_data.load_data(subject, task)
             filter_raw = s_01_filter.filter(raw)
-            print(raw)
+            #print(raw)
             resample_raw = s_02_downsample.downsample_data(raw, config.sample_rate)
+            s_03_remove_bad_channels.remove_bad_channels(resample_raw)
             print("success!")
