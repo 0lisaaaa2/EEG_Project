d9d3e92f6b7a2d6564c5225b969b1219a2007b26
Lisa Fabian <lisa@pop-os.localdomain>
2025-12-11 13:51:20 +0100

    Filterfuktion angepasst

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from config import subjects, tasks
 import s_00_fetch_data
+import s_01_filter
 
 if __name__ == "__main__":
     for subject in subjects:
         for task in tasks:
             raw = s_00_fetch_data.load_data(subject, task)
+            filter_raw = s_01_filter.filter(raw)
             print(raw)
--- s_01_filter.py
+++ s_01_filter.py
@@ -2,24 +2,28 @@
 
 import matplotlib.pyplot as plt
 import numpy as np
-
 import mne
-
 import config
-import s_00_fetch_data
 
 
-def frequencyspectrum(raw_before, raw_after, fmax=100):
-    psd_before, freqs = raw_before.compute_psd(fmax=fmax)
-    psd_after, _ = raw_after.compute_psd(fmax=fmax)
+def frequencyspectrum(raw_before_filter, raw_after_highlow, raw_after_notch, fmax=100):
+    psd_before_filter = raw_before_filter.compute_psd(fmax=fmax)
+    psd_after_highlow = raw_after_highlow.compute_psd(fmax=fmax)
+    psd_after_notch = raw_after_notch.compute_psd(fmax=fmax)
 
     # Mittelwert über alle Kanäle
-    psd_before_mean = psd_before.get_data().mean(axis=0)
-    psd_after_mean = psd_after.get_data().mean(axis=0)
+    psd_before_filter_mean = psd_before_filter.get_data().mean(axis=0)
+    psd_after_highlow_mean = psd_after_highlow.get_data().mean(axis=0)
+    psd_after_notch_mean = psd_after_notch.get_data().mean(axis=0)
+
+    freqs_1 = psd_before_filter.freqs
+    freqs_2 = psd_after_highlow.freqs
+    freqs_3 = psd_after_notch.freqs
 
     plt.figure(figsize=(10, 5))
-    plt.plot(freqs, 10 * np.log10(psd_before_mean), label='Before Filtering')
-    plt.plot(freqs, 10 * np.log10(psd_after_mean), label='After Filtering')
+    plt.plot(freqs_1, 10 * np.log10(psd_before_filter_mean), label='Before Filtering')
+    plt.plot(freqs_2, 10 * np.log10(psd_after_highlow_mean), label='After HighLowPass Filter')
+    plt.plot(freqs_3, 10 * np.log10(psd_after_notch_mean), label='After Notch Filter')
     plt.xlabel("Frequency (Hz)")
     plt.ylabel("Power (dB)")
     plt.title("Power Spectrum Before vs After Filtering")
@@ -43,8 +47,8 @@ def notch(raw):
 
 
 
-if __name__ == "__main__":
-    raw = s_00_fetch_data.load_data("001", "regfront")
-    #raw_after_highlow = highlowpass(raw_before)
-    #frequencyspectrum(raw, raw_after_after_highlow)
-    #raw_after_notch = notch(raw_after)
+def filter(raw):
+    # raw = s_00_fetch_data.load_data("001", "regfront")
+    raw_after_highlow = highlowpass(raw)
+    raw_after_notch = notch(raw_after_highlow)
+    frequencyspectrum(raw, raw_after_highlow, raw_after_notch)
