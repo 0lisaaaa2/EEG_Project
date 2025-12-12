a88ae709519bd99d1341d54fc40ad8f96b90b827
Lisa Herrmann <herrmann.lisa02@gmail.com>
2025-12-12 15:44:44 +0100

    finish epoching

--- s_07_epochs.py
+++ s_07_epochs.py
@@ -1,14 +1,15 @@
 import mne
 
-def epoching(raw, tmin=-0.2, tmax=0.8):
-    """Epoch the raw data around events.
+"""Epoch the raw data around events.
 
     input: raw (EEG Data), event_id (dict of event labels and their IDs), tmin (start time before event), tmax (end time after event)
     return: epochs (Epoched EEG Data)
     """
-    events, event_id = mne.events_from_annotations(raw)
-    print("evnets", events)
-    print("event_id", event_id)
+def epoching(raw, tmin=-0.2, tmax=0.8):
 
-    epochs = mne.Epochs(raw, events, event_id, tmin, tmax, reject_by_annotation=True)
+    events, event_id = mne.events_from_annotations(raw)
+    #print("evnets", events)
+    #print("event_id", event_id)
+
+    epochs = mne.Epochs(raw, events, event_id, tmin, tmax, reject_by_annotation=True, event_repeated='drop')
     return epochs
