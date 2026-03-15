e0814f64ad2a42db3026d8058988bedb82f199e8
Lisa Fabian <lisa@pop-os.localdomain>
2026-03-15 18:24:52 +0100

    additional testing

--- s_14_additional_stat.py
+++ s_14_additional_stat.py
@@ -1,9 +1,26 @@
-
+import config
+import scipy.stats as stats
+from scipy.stats import t
 
 def mean_amplitude(evoked, roi, tmin, tmax):
     evoked_roi = evoked.copy().pick_channels(roi)
     evoked_crop = evoked_roi.crop(tmin=tmin, tmax=tmax)
     return evoked_crop.data.mean()
 
-def additional_stats(evoked_front_sym, evoked_pers_sym, evoked_front_asym, evoked_pers_asym):
-    
+def erp_to_amplitude(evoked_front_sym, evoked_pers_sym, evoked_front_asym, evoked_pers_asym):
+    roi = config.posterior_channels
+    tmin = config.tmin_spn
+    tmax = config.tmax_spn
+
+    front_sym_val = mean_amplitude(evoked_front_sym, roi, tmin, tmax)
+    pers_sym_val = mean_amplitude(evoked_pers_sym, roi, tmin, tmax)
+
+    front_asym_val = mean_amplitude(evoked_front_asym, roi, tmin, tmax)
+    pers_asym_val = mean_amplitude(evoked_pers_asym, roi, tmin, tmax)
+
+    return (front_sym_val, pers_sym_val, front_asym_val, pers_asym_val)
+
+
+def additional_t_test(pers_val, front_val, symasym):
+    res= stats.ttest_rel(pers_val, front_val)
+    print(f"{symasym}: t={res.statistic}, p={res.pvalue}")
